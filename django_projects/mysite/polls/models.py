9e6386d0b7a68a5afaa8fa96ea7a77ac04121e2c
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2022-06-30 17:50:52 -0500

    Creacion modelos Question y Choice

--- django_projects/mysite/polls/models.py
+++ django_projects/mysite/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    # Django automaticamente genera una llave primaria, por eso
+    # no se necesita crearla.
+    question_text = models.CharField(max_length=255) # CharField = Varchar
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    # CASCADE: Cuando se borre una pregunta, se borraran todas las choices que esa pregunta tenga
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
