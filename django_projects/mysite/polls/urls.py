1d008015639f1e313d07daf734f7ab30c9df8ee8
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2022-07-02 14:27:39 -0500

    Modularizando polls/views y templates/polls/index.html

--- django_projects/mysite/polls/urls.py
+++ django_projects/mysite/polls/urls.py
@@ -7,9 +7,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
     path('', views.index, name='index'), # "/polls/"
-    path('<int:question_id>', views.detail, name='detail'), # example: "/polls/detail/2"
+    path('detail/<int:question_id>', views.detail, name='detail'), # example: "/polls/2"
     path('results/<int:question_id>', views.results, name='results'), # example: "/polls/results/2"
-    path('vote/<int:question_id>', views.vote, name='vote'), # example: "/polls/votes/2"
+    path('vote/<int:question_id>', views.vote, name='vote'), # example: "/polls/vote/2"
 ]
