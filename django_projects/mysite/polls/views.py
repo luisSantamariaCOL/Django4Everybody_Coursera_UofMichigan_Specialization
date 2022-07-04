cca49ed2a54219c21e4d541a6d7ab9886bcb4d56
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2022-07-03 21:06:54 -0500

    Creacion de vista vote

--- django_projects/mysite/polls/views.py
+++ django_projects/mysite/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
+from .models import Question, Choice
 
 # function based view
 def index(request):
@@ -21,4 +22,21 @@ def results(request, question_id):
     return HttpResponse(f"You are seeing the results of the question number {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"You are voting the question number {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    
+    # Here, we are going to get the user answer from the form (in polls/detail.html)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", { 
+            "question": question,
+            "error_message": "An answer was not provided"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save() # Save the current object state in the db
+        
+        # HttpResponseRedirect ensures that the user does not send the same
+        # information twice
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        
